5c0d051329089e47c3b17fd32106f3a6ef197b3f
Baconrad <sacredhawk@gmail.com>
2023-08-23 23:03:48 +0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,45 @@
+const clickQueue = []
+let isClicking = false
+
+const liveChange = () => {
+    let targetNode = document.querySelector('.results')
+    let observer = new MutationObserver((mutations, observer) => {
+        mutations.forEach((mutation) => {
+            mutation.addedNodes.forEach((node) => {
+                if (node.className === 'row') {
+                    let WhisperBtn = node.querySelector('.direct-btn')
+                    if (WhisperBtn.disabled === false) {
+                        clickQueue.push(WhisperBtn);
+                        if (!isClicking) {
+                            processClickQueue()
+                        }
+                    }
+                }
+            })
+        })
+    })
+    observer.observe(targetNode, { childList: true, subtree: true })
+}
+
+function processClickQueue() {
+    if (clickQueue.length > 0) {
+        isClicking = true
+        const btn = clickQueue.shift()
+        simulateButtonClickWithDelay(btn, 1000, () => {
+            processClickQueue()
+        })
+    } else {
+        isClicking = false
+    }
+}
+
+function simulateButtonClickWithDelay(element, delay, callback) {
+    setTimeout(() => {
+        element.click()
+        if (callback) {
+            callback()
+        }
+    }, delay)
+}
+
+liveChange()
